1dabed7eb8d125f184c8e9212c8fa5ea0316ed8d
Muhammad Fadli Faturzaman <muhammad.fadli.f.z@gmail.com>
2016-03-21 23:47:54 +0700

    nambah tulisan

--- Sisfo Perpus/src/model/SisfoPerpus.java
+++ Sisfo Perpus/src/model/SisfoPerpus.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,39 +28,9 @@ public class SisfoPerpus {
         
         member.rateBuku();
         System.out.println("ini menu");
+        System.out.println("ini mantap");
     }
     
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
-
-/**
- *
- * @author ASUS
- */
-    import java.util.*;
-
-public class SisfoPerpus {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Admin admin = new Admin();
-        Buku buku = new Buku();
-        Kategori katalog = new Kategori();
-        Member member = new Member();
-        Peminjaman peminjaman = new Peminjaman();
-        Pengunjung pengunjung = new Pengunjung();
-        
-        member.rateBuku();
-    }
     
 }
->>>>>>> origin/master
+
